46efe8649ee6d7bd60d2168b1d7bd4b36be15837
PadmajaNaik14 <padmajagnaik@gmail.com>
2026-04-15 09:18:25 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,17 +11,17 @@ public class App
         WebDriver driver = new ChromeDriver();
 
         try {
-            driver.get("https://practicetestautomation.com/practice-test-login/");
+            driver.get("https://www.saucedemo.com/");
             driver.manage().window().maximize();
 
             Thread.sleep(2000); // wait 2 seconds after page load
 
-            driver.findElement(By.id("username")).sendKeys("student");
-            driver.findElement(By.id("password")).sendKeys("Password123");
+            driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
 
             Thread.sleep(2000); // wait before clicking login
 
-            driver.findElement(By.id("submit")).click();
+            driver.findElement(By.id("login-button")).click();
 
             Thread.sleep(2000); // wait after login
 
